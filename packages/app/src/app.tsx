e0a8bd8512175bbc7872db9b8b400b11eaab356a
bestlyg <bestlyg@foxmail.com>
2023-12-08 16:23:20 +0800

    feat: update app

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,2 +1,7 @@
-import { Button } from "@component-type-checker/sdk";
+// import { Button } from "@component-type-checker/components";
+import { VERSION } from "@component-type-checker/sdk";
+import { Button } from "@component-type-checker/components";
 import { createButton } from "@component-type-checker/button";
+
+console.log(VERSION);
+const button: Button = createButton();
